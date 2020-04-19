c9c6eb71f0da5e371dcafc79fc7c93f56255d75e
Ubuntu <ubuntu@ip-172-31-27-213.us-west-2.compute.internal>
2020-04-19 03:37:30 +0000

    added home page with 2 buttons

--- server.py
+++ server.py
@@ -1,14 +1,41 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
+import json
 
 class myHandler(BaseHTTPRequestHandler):
         
-    #Handler for the GET requests
-    def do_GET(self):
+    def serve_home_page(self):
         self.send_response(200)
         self.send_header('Content-type','text/html')
         self.end_headers()
-        self.wfile.write(b"Hello World !")
+        page = self.read_page("index.html")
+        self.wfile.write(page.encode())
+
+    #Handler for the GET requests
+    def do_GET(self):
+        print (self.path)
+        if (self.path.endswith("home.html")):
+            self.serve_home_page()
+        elif (self.path.endswith("server_on?")):
+            self.start_server()
         return
+
+    def start_server(self):
+        self.send_response(200)
+        self.send_header('Content-type','application/json')
+        self.end_headers()
+        ip_addr = get_ip_addr()
+        self.wfile.write(json.dumps({'ip': ip_addr}))
+
+    def get_ip_addr(self):
+        return '0.0.0.0'
+
+    def read_page(self, file_name):
+        with open(file_name) as f:
+            read_data = f.read();
+            print (read_data)
+            return read_data
+
+    
 
 def run(port_number=8000):
     server = HTTPServer(('', port_number), myHandler)
